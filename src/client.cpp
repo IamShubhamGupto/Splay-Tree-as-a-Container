da2b886f2cf3cd086994e9c9c17fac7173337d69
Shubham <shubhamgupto@gmail.com>
2021-04-02 12:50:59 +0530

    a better client file, broke implementation

--- src/client.cpp
+++ src/client.cpp
@@ -6,13 +6,52 @@
 using namespace std;
 
 int main(){
-    SplayTree<int, double> splaytree;
-    splaytree.insert(1,6.66);
-    splaytree.insert(2,9.9999);
-    splaytree.display();
-    splaytree.insert(10,43.2);
-    cout << splaytree.find(2) << "\n";
-    splaytree.insert(5,123.1);
-    splaytree.display();
+    {
+        SplayTree<int, double> splaytree;
+        splaytree.insert(1,6.66);
+        splaytree.insert(2,9.9999);
+        splaytree.display();
+        splaytree.insert(10,43.2);
+        cout << splaytree.find(2) << "\n";
+        cout << splaytree.find(6) << "\n";
+        splaytree.insert(5,123.1);
+        splaytree.display();
+    }
+
+    {
+        SplayTree<int, int> splaytree;
+        splaytree.insert(1,6);
+        splaytree.insert(2,9);
+        splaytree.display();
+        splaytree.insert(10,43);
+        cout << splaytree.find(2) << "\n";
+        cout << splaytree.find(6) << "\n";
+        splaytree.insert(5,123);
+        splaytree.display();
+    }
+    
+    {
+        SplayTree<char*, int> splaytree;
+        splaytree.insert("hi",1);
+        splaytree.insert("there",2);
+        splaytree.display();
+        splaytree.insert("hey",2);
+        cout << splaytree.find("there") << "\n";
+        cout << splaytree.find("hola") << "\n";
+        splaytree.insert("hello",5);
+        splaytree.display();
+    }
+
+    {
+        SplayTree<int, char*> splaytree;
+        splaytree.insert(1,"hi");
+        splaytree.insert(2,"there");
+        splaytree.display();
+        splaytree.insert(10,"hey");
+        cout << splaytree.find(2) << "\n";
+        cout << splaytree.find(6) << "\n";
+        splaytree.insert(5,"hello");
+        splaytree.display();
+    }
     return 0;
 }
